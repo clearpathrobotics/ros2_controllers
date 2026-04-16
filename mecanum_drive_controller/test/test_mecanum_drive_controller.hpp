13fbc9541250adac5d9fdb2f1a5bf9b92af08da2
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-04-16 13:16:56 -0400

    Added velocity limiting to the mecanum controller.

--- mecanum_drive_controller/test/test_mecanum_drive_controller.hpp
+++ mecanum_drive_controller/test/test_mecanum_drive_controller.hpp
@@ -91,6 +91,11 @@ class TestableMecanumDriveController : public mecanum_drive_controller::MecanumD
   FRIEND_TEST(MecanumDriveControllerTest, configure_succeeds_tf_test_prefix_true_set_namespace);
   FRIEND_TEST(MecanumDriveControllerTest, configure_succeeds_tf_blank_prefix_true_set_namespace);
 
+  FRIEND_TEST(MecanumDriveControllerTest, test_no_speed_limiter_when_not_configured);
+  FRIEND_TEST(MecanumDriveControllerTest, test_speed_limiter_linear_x);
+  FRIEND_TEST(MecanumDriveControllerTest, test_speed_limiter_linear_y);
+  FRIEND_TEST(MecanumDriveControllerTest, test_speed_limiter_angular_z);
+
 public:
   controller_interface::CallbackReturn on_configure(
     const rclcpp_lifecycle::State & previous_state) override
