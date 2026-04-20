31036af4213edd6c6946b0ae39b9e2009aa81e11
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-04-20 19:22:26 -0400

    Added reset for previous values on lifecycle changes and tests for it.

--- mecanum_drive_controller/include/mecanum_drive_controller/mecanum_drive_controller.hpp
+++ mecanum_drive_controller/include/mecanum_drive_controller/mecanum_drive_controller.hpp
@@ -68,6 +68,8 @@ public:
   controller_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
+  void reset_buffers();
+
   controller_interface::return_type update_reference_from_subscribers(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
@@ -154,6 +156,8 @@ private:
   std::unique_ptr<control_toolbox::RateLimiter<double>> limiter_linear_x_;
   std::unique_ptr<control_toolbox::RateLimiter<double>> limiter_linear_y_;
   std::unique_ptr<control_toolbox::RateLimiter<double>> limiter_angular_z_;
+
+protected:
   // Previous two commands for jerk limiting: queue of [linear_x, linear_y, angular_z]
   std::queue<std::array<double, 3>> previous_two_commands_;
 };
